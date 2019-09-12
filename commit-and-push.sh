28811e2e64d3b8dbfd3a2278ab5a831b27327db5
Oliver Davies <oliver@oliverdavies.uk>
2019-09-12 21:49:46 +0100

    Live blogging from SymfonyLive London 2019

--- commit-and-push.sh
+++ commit-and-push.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-git add -A \
-  && git commit -m "$1" \
-  && git push
+git add -A
+git commit -m "$1"
+git push
+
+
+nodemon \
+    --watch ./output_dev \
+    --ext html \
+    --exec './commit-and-push.sh "Live blogging from SymfonyLive London 2019"'
